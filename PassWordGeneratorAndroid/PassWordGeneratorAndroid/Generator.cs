d5a7e4656948932287d1ad087972580a3c67a2cd
danteCLRK <fryanslaucerers@gmail.com>
2018-01-19 11:42:40 -0600

    Added prime number checker to Generator class.

---
+++ PassWordGeneratorAndroid/PassWordGeneratorAndroid/Generator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PassWordGeneratorAndroid
+{
+    public static class Generator
+    {
+        public static string generatePassNum(string salt)
+        {
+            return salt;
+        }
+
+        public static bool isPrime(int n)
+        {
+            bool prime = true;
+
+            if(n <= 1)
+            {
+                prime = false;
+            }
+            else if(n <= 3)
+            {
+                prime = true;
+            }
+            else
+            {
+                int i = 5;
+                while(i*i <= n)
+                {
+                    if(n % i == 0 || n % (i+2) == 0)
+                    {
+                        prime = false;
+                    }
+                }
+            }
+
+            return prime;
+        }
+    }
+}
